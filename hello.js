188c1f6eb2b92a1887f3be60a1c19b8e044f32b1
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-08-07 22:11:00 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,29 @@
+// var _a = "atik"
+/* console.log(_a);
+ var $a = "atik is wise man"
+ console.log($a);*/
+
+/*var text = "hello " + "atik";
+var welcome= "Ki khobor " + text;
+var number = 10;
+var number= "tarek";
+
+console.log(welcome);
+
+console.log(typeof(welcome));
+console.log(number)*/
+
+var a = 10;
+var b = "10";
+
+console.log(typeof (a));
+console.log(typeof (b));
+if (a == b) {
+
+    console.log("true");
+}
+else {
+
+    console.log("false");
+
+}
