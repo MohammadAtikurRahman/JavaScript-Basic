932c6d56837f00161d9b3a84ca88e035e3a82be7
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-08-08 18:07:53 +0600

    adding more with day3

--- arrowfunction.js
+++ arrowfunction.js
@@ -1,14 +1,18 @@
- function evenNumber(){
+function evenNumber() {
 
-console.log("This is Even Number");
+    console.log("This is Even Number");
 
 
 }
 evenNumber();
 
-let oddFun = ()=>{
-   console.log("This is Odd Number");
+let oddFun = () => {
+    console.log("This is Odd Number");
 
 }
 
 oddFun();
+
+
+let number = () => 10; // 10 as a return
+console.log(number()); // number is varible name
