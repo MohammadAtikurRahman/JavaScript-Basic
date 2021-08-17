1e06a8fde083d8ffc0e06aedca5428baa1cd33a7
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-08-17 22:36:21 +0600

    two types of

---
+++ mult.js
@@ -0,0 +1,8 @@
+function mult(x, y) {
+
+    z = x * y;
+    console.log(z);
+
+
+}
+mult(100, 2);
---
+++ subtraction.js
@@ -0,0 +1,9 @@
+function subtraction(x, y) {
+
+    z = x - y;
+    console.log(x + " Subtracted to " + y + " is equal to " + z);
+
+
+}
+
+subtraction(50, 25);
