4c9325d53da83cbf518075f28f7ac55f9659121e
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-08-30 23:20:34 +0600

    hk8

---
+++ hk8.js
@@ -0,0 +1,30 @@
+function vowelsAndConsonants(str) {
+
+
+    // console.log(`${str} ${str.length}`);
+
+    var start1 ;
+    var start2 = 1;
+    //var i =0
+  
+    
+
+
+    for (start1 = 0; start1 <= str.length; start1++,start2++){
+
+
+        var result1 = str.slice(start1,start2);
+   
+     
+       
+
+      //  console.log(result1);
+    }
+
+   
+  
+
+
+}
+
+vowelsAndConsonants("TAIK");
