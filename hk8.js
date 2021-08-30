11c087512dafaa42a1631e842427fbe74171d2d7
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-08-30 23:40:17 +0600

    added 8

--- hk8.js
+++ hk8.js
@@ -4,27 +4,46 @@ function vowelsAndConsonants(str) {
     // console.log(`${str} ${str.length}`);
 
     var start1 ;
+
+    var s;
+    var s2 =1;
     var start2 = 1;
     //var i =0
-  
+     var track = str;
+    
+    const vowels = str.match(/[aeiou]/gi);
+    const consonants = str.match(/[^aeiou]/gi);
+   
     
 
+    
+    for (start1 = 0; start1 < vowels.length; start1++,start2++){
 
-    for (start1 = 0; start1 <= str.length; start1++,start2++){
 
-
-        var result1 = str.slice(start1,start2);
-   
-     
+        var result1 = vowels.slice(start1,start2);
        
-
-      //  console.log(result1);
-    }
-
    
-  
+      
+        console.log(result1);
+        
+
+      
+    }
+    for (s = 0; s < consonants.length; s++, s2++) {
+
+
+      
+        var result2 = consonants.slice(s, s2);
+
+
+        console.log(result2);
+
+
+
+    }
+   
 
 
 }
 
-vowelsAndConsonants("TAIK");
+vowelsAndConsonants("javascriptloops");
