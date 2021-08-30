d0bd62987734f26a2c67931aa9f8692c88af2ea7
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-08-30 22:31:14 +0600

    added 7

--- switch.js
+++ switch.js
@@ -1,15 +1,26 @@
-var day = "Sunday";
-switch(day){
-   
-    case 'Saturday':
-        console.log("Tdoay is Saturday");
-        break;
-    case 'Sunday':
-        console.log("Today is Sunday");
-        break;
-    default:
-        console.log("Today is nothing");
-      
+
+function getLetter(s) {
+    let letter;
+    
+   switch(s){
+                         
+
+       case 'adfgt':
+           console.log(s);
+           break;
+    
 
 
+
+
+
+
+
+   }
+
+
+
+
+    return letter;
 }
+getLetter("adfgt");
