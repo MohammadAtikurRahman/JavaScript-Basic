1db033a50258236ce7ef0f58af2fa953a11caca2
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-08-31 23:31:01 +0600

    complex

--- hk9.js
+++ hk9.js
@@ -1,20 +1,21 @@
 function getSecondLargest(nums) {
     
-  
-   for (var i = 0; i < nums.length; i++) {
-     
-       let maxmNumber = Math.max(...nums);
+
+    let lowestToHighest = nums.sort((a, b) => b - a);
+
+    let secondLargestValue = lowestToHighest.slice(1, 2)
+
+    let finalResult = secondLargestValue.toString()
+   
+    
+    console.log(finalResult);
 
 
-       console.log(maxmNumber);
 
-
-
-    }
 
 
 }
 
 
-var x = ['2', '3', '6','6','5'];
+var x = ['2', '3', '6','6','5','200','19'];
 getSecondLargest(x);
