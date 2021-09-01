ead742520fa08b59fd03ef0f6ad11b32af505ac8
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-01 22:36:42 +0600

    alt

---
+++ maxnum.js
@@ -0,0 +1,26 @@
+function getSecondLargest(nums) {
+
+
+
+    var secondLargest = [];
+    var maxmNumber = [];
+    var secondMaxmNumber = [];
+    maxmNumber = Math.max(...nums);
+    console.log("First Maxnumber " + maxmNumber);
+
+ 
+
+    for (var i = 0; i < nums.length; i++) {
+
+     
+    }
+
+   
+
+
+
+}
+
+
+var x = ['1', '2', '34', '41', '510', '6'];
+getSecondLargest(x);
