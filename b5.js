35db80f0bf2e82f62788e2cef4eff7c42b48c05f
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-06 13:58:30 +0600

    object

---
+++ b5.js
@@ -0,0 +1,34 @@
+var atik = {
+    fullName: 'Md. Atikur Rahman',
+    age: 30,
+    address: 'dhaka',
+    job: 'Software Engineer'
+}
+
+var seemi = {
+    fullName: 'Seemi Tasnim Alam',
+    age: 30,
+    address: 'Dhaka',
+    job: 'Asst. Professor'
+}
+var tarek = {
+fullName: 'Md. Nazmul Ahsan',
+   age: 30,
+   address: 'Dhaka',
+   job: 'Sr. Senior Software engineer'
+}
+var anonna ={
+   fullName: 'Mst. Israt Jahan',
+   age: 24,
+   address: 'Pabna',
+   job: 'Student'
+}
+console.log(atik);
+console.log(seemi);
+console.log(tarek);
+console.log(anonna);
+
+console.log(atik.fullName);
+console.log(tarek.job);
+console.log(seemi.age);
+
