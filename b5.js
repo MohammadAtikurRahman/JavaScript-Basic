7c0cacef621c54b86181c79916f30db52be9910a
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-06 15:55:03 +0600

    long

--- b5.js
+++ b5.js
@@ -27,8 +27,41 @@ console.log(atik);
 console.log(seemi);
 console.log(tarek);
 console.log(anonna);
-
+atik.zipCode = 3517;
+atik.address='Mohammadpur';
 console.log(atik.fullName);
 console.log(tarek.job);
 console.log(seemi.age);
+console.log(atik.zipCode);
+console.log(atik.address);
+// object er moddhe function k rakhte chaile.. onek ta atik.zipcode er moto kore kisu akta rekha deo item hisbae
+seemi.LoveText = function (){
+    console.log('Do something best in the job');
+}
+seemi.LoveText();
+atik.welcomeMsg = function(){
+    console.log('Welcome to function as item in object.');
+}
+atik.welcomeMsg();
+tarek.Jobmsg = function (){
+         console.log('JavaScript is the key of sucess');
+}
+tarek.Jobmsg();
+atik.EvenFinder = function(num){
+       if(num%2==0){
+            console.log('This is even Number');
+        }
+        else{
+            console.log('This is Odd Number');
+        }
+}
+atik.EvenFinder(5);
+atik.addTwoNumber = function(a,b){
+        var c = a + b;
+        console.log('addition of two number ' +c);
+}
+atik.addTwoNumber(10,20);
+
+
+
 
