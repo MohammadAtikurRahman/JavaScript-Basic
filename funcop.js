642559c8ab8ef6c3337fc1059d3d3a664dbcc78c
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-06 22:09:04 +0600

    inside

--- funcop.js
+++ funcop.js
@@ -29,3 +29,15 @@ function display(name){
 
 }
 display('Md. Atikur Rahman')('Software Engineering');
+
+function changeValue(a) {
+    console.log('Value of a inside function before changing: ' + a);
+    a = 10;
+    console.log('Value of a inside function after changing: ' + a);
+    return a;
+}
+
+var a = 100;
+console.log('Value of a before changing function is applied: ' + a);
+changeValue(a);
+console.log('Value of a after changing function is applied: ' + a);
