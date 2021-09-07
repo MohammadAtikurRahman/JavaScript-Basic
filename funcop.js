016526ccf98fab2d48e3ebae64be69f7dce6e18e
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-07 15:35:14 +0600

    another

--- funcop.js
+++ funcop.js
@@ -31,13 +31,69 @@ function display(name){
 display('Md. Atikur Rahman')('Software Engineering');
 
 function changeValue(a) {
-    console.log('Value of a inside function before changing: ' + a);
+
+    console.log('1  Value of a inside function before changing: ' + a);
     a = 10;
-    console.log('Value of a inside function after changing: ' + a);
+    console.log('2  Value of a inside function after changing: ' + a);
     return a;
 }
 
 var a = 100;
-console.log('Value of a before changing function is applied: ' + a);
+console.log('3  Value of a before changing function is applied: ' + a);
 changeValue(a);
-console.log('Value of a after changing function is applied: ' + a);
+console.log('4 Value of a after changing function is applied: ' + a);
+
+
+
+function clouserDemo(){
+
+     var a = 10;
+
+
+    return function(){
+          
+        var b = 20;
+        var c = a + b ;
+        console.log(c);
+
+    }
+}
+//clouserDemo()();
+
+var aVr= clouserDemo();
+var total = aVr();
+console.log(total);
+
+function sEEmi(){
+
+  console.log('working test');
+
+
+}
+
+sEEmi();
+
+
+var faisal = function() {
+    console.log('Today Faisal will come to my house');
+}
+faisal();
+
+
+var friends = function() {
+         console.log('Today another friend come to my house')
+}
+friends();
+
+//Immediately Invoked Function Expressions (IIFE)
+
+(function happy() {
+    
+    console.log('I am happy person');
+})();
+
+(function iiFeTest(){
+  console.log('testing this project');
+
+
+})(); 
