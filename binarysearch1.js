a4833569673ba638130e312afd6e25b9b6fec804
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-08 22:48:34 +0600

    map

--- binarysearch1.js
+++ binarysearch1.js
@@ -1,24 +1,24 @@
-function  bSearch(nums) {
-    
+function bSearch(nums) {
+
     console.log(nums);
     var len = nums.length;
     console.log(len);
     var obj = {
-        
-        left : 0,
-        right : len - 1
-        
+
+        left: 0,
+        right: len - 1
+
     }
     var left = obj.left;
     var right = obj.right;
     console.log(left);
     console.log(right);
-      
+
 
 
 
 }
 
 
-var arr = ['1','2','3','4','5'];
+var arr = ['1', '2', '3', '4', '5'];
 bSearch(arr);
---
+++ map.js
@@ -0,0 +1,5 @@
+const map1 = new Map();
+
+map1.set('Atik',30);
+map1.set('Tarek',30);
+map1.set('Anonna',25);
