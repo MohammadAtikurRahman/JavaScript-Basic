6f41ca852d4fd406c796cc3abad1e3b180fe97f3
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-08 22:09:20 +0600

    added

---
+++ binarysearch1.js
@@ -0,0 +1,24 @@
+function  bSearch(nums) {
+    
+    console.log(nums);
+    var len = nums.length;
+    console.log(len);
+    var obj = {
+        
+        left : 0,
+        right : len - 1
+        
+    }
+    var left = obj.left;
+    var right = obj.right;
+    console.log(left);
+    console.log(right);
+      
+
+
+
+}
+
+
+var arr = ['1','2','3','4','5'];
+bSearch(arr);
