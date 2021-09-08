5a48e690b92d9f4946c896f787f86d780db5631a
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-08 22:51:43 +0600

    added more

--- map.js
+++ map.js
@@ -1,5 +1,15 @@
 const map1 = new Map();
 
-map1.set('Atik',30);
-map1.set('Tarek',30);
-map1.set('Anonna',25);
+map1.set('atik',30);
+map1.set('tarek',30);
+map1.set('anonna',25);
+map1.set('moin' , 19);
+
+var a = map1.get('atik');
+var b = map1.get('tarek');
+var c = map1.get('anonna');
+var d = map1.get('moin')
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
