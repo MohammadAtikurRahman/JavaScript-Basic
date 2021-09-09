2db592e2de5677295c504d207f5f523b19739b15
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-09 22:52:13 +0600

    ok

---
+++ min.js
@@ -0,0 +1,15 @@
+(function min(x,y) {
+     
+   if( x > y ){
+            
+             console.log(`${y} is minmum number `);
+
+   }
+   else{
+
+            console.log(`${y} is maximum  number `);
+   }
+  
+      
+
+})(10,7);
--- objaray.js
+++ objaray.js
@@ -1,10 +1,10 @@
 var obj = {
-   
-    name: ['atik','tarek','anonna'],
-    age : ['30','30','25'],
+
+    name: ['atik', 'tarek', 'anonna'],
+    age: ['30', '30', '25'],
     address: 'Pabna'
 
-} 
-console.log('Arry in the object ' +obj.name[2])
-console.log('Arry in the object ' +obj.name[0]);
-console.log('Single Printed ' +obj.address)
+}
+console.log('Arry in the object ' + obj.name[2])
+console.log('Arry in the object ' + obj.name[0]);
+console.log('Single Printed ' + obj.address)
