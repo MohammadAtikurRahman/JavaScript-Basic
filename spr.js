ca123501b58c1de557a7883e0363dcc61c5adbaf
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-14 22:03:02 +0600

    w

--- spr.js
+++ spr.js
@@ -4,4 +4,6 @@ var numbers = [1, 2, 3, 4];
 function calculate(a , b, c , d) {
     console.log('Sum: ' + (a + b + c + d));
 }
+
+//speard use korar maddhome amra arry k venge fellam 
 calculate(...numbers);
