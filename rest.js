b540e7a4bbb3fdc9634db125a16814de8cceb58c
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2021-09-14 22:34:03 +0600

    ok

--- rest.js
+++ rest.js
@@ -7,3 +7,7 @@ function  moreArgu() {
 }
 moreArgu('atik','tarek','anonna');
 // arrow function e kintu arguments kaj korbe na.. eita sudhu es5 er jonno
+
+//argumentsঅবজেক্ট ঠিক অ্যারে না হওয়ায় এখানে আপনি চাইলে 
+//forEach, map, filter, reduceবা এরকম বাকী যত মেথড আছে 
+//সেগুলো কাজ করাতে পারবেন নাঃ
