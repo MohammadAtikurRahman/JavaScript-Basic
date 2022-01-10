126aa62af654af4c5a64ec2e3b0f6a7876e74368
Md. Atikur  Rahman <contact.atikurrahman@gmail.com>
2022-01-10 23:38:41 +0600

    customthemeforreact

---
+++ customthemereact.js
@@ -0,0 +1,25 @@
+const theme = createTheme({
+  
+  palette: {
+
+    primary:{
+
+      main: '#fefefe'
+    },
+
+    secondary: purple
+
+      
+    
+  },
+  typography:{
+     fontFamily: 'Quicksand',
+     fontWeightLight: 400,
+     fontWeightRegular: 500,
+     fontWeightMedium: 600,
+     fontWeightBold: 700
+
+
+  }
+
+})
